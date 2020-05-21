d65559b0ddee197b7486d72e6b0e4d991899e158
Nishil <nishilkapadia17@gmail.com>
2020-05-22 03:26:23 +0530

    :fire: Logout added

--- controllers/auth.js
+++ controllers/auth.js
@@ -47,6 +47,23 @@ exports.login = asyncHandler(async (req, res, next) => {
 	sendTokenResponse(user, 200, res);
 });
 
+//@desc :       Logout
+//@route :   GET /api/v1/auth/logot
+//@access:          Private
+exports.logout = asyncHandler(async (req, res, next) => {
+	res.cookie('token', 'none', {
+		expires: new Date(Date.now() + 10 * 1000),
+		httpOnly: true,
+	});
+
+	// res.clearCookie('token');
+
+	res.status(200).json({
+		success: true,
+		data: {},
+	});
+});
+
 //@desc :       Current Logged in user
 //@route :   GETs /api/v1/auth/me
 //@access:          Private
--- routes/auth.js
+++ routes/auth.js
@@ -7,12 +7,14 @@ const {
 	resetPassword,
 	updateDetails,
 	updatePassword,
+	logout,
 } = require('../controllers/auth');
 const { protect } = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/getme', protect, getMe);
+router.get('/logout', logout);
 
 router.post('/register', register);
 router.post('/forgotpassword', forgotpassword);
