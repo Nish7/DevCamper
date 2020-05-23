d79b52d43210096c6076dcb19090d4559e834931
Nishil <nishilkapadia17@gmail.com>
2020-05-24 03:54:21 +0530

    make it better

--- server.js
+++ server.js
@@ -79,12 +79,10 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
-	console.log(
-		`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
-	);
+	console.log(`Server running on ${process.env.NODE_ENV} mode on port ${PORT}`);
 });
 
 process.on('unhandledRejection', (err, promise) => {
-	console.log(`Error: ${err.message}`.red);
+	console.log(`Error: ${err.message}`);
 	server.close(() => process.exit(1));
 });
